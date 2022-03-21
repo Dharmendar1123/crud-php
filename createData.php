4f6426547e1151de41174a82c6e2fe72a408b6f2
Dharmendar1123 <dharmendar1123@gmail.com>
2022-03-21 13:35:08 +0530

    initial commit

---
+++ createData.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title>Create</title>
+</head>
+
+<body>
+    <?php
+
+    $dbServerName = "localhost";
+    $dbUserName = "root";
+    $dbPassword = "";
+    $dbName = "userdata";
+
+    $firstName = $lastName = $email = $password = $userAddedSuccess = "";
+    $firstNameError = $lastNameError = $emailError = $passwordError = "";
+    $flag = true;
+
+    $connection = new mysqli($dbServerName, $dbUserName, $dbPassword, $dbName);
+
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if (empty($firstName)) {
+            $firstNameError = "First Name should not be null empty";
+            $flag = false;
+        }
+        if (empty($lastName)) {
+            $lastNameError = "Last Name should not be null empty";
+            $flag = false;
+        }
+        if (empty($email)) {
+            $emailError = "Email Should not be null empty";
+            $flag = false;
+        }
+        if (empty($password)) {
+            $passwordError = "Password should not be null empty";
+            $flag = false;
+        }
+        if ($flag == true) {
+
+            $sql = "INSERT INTO users (firstname, lastname, email, password)
+                VALUES ('$firstName', '$lastName', '$email', '$password')";
+
+            if ($connection->query($sql) === FALSE) {
+                echo "Error: " . $sql . "<br>" . $connection->error;
+            }
+            $userAddedSuccess = "User Added Successfully";
+            $connection->close();
+
+            header('location: readData.php');
+        }
+    }
+
+    ?>
+    <div class="createDataCard">
+        <h1>Welcome</h1>
+        <h3>Let's Create your Account</h3>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input class="inputField" type="text" id="firstName" name="firstName" placeholder="First Name"><br>
+            <span class="errorMsg"><?php echo $firstNameError; ?></span><br>
+
+            <input class="inputField" type="text" id="lastName" name="lastName" placeholder="Last Name"><br>
+            <span class="errorMsg"><?php echo $lastNameError; ?></span><br>
+
+            <input class=" inputField" type="email" id="email" name="email" placeholder="Email"><br>
+            <span class="errorMsg"><?php echo $emailError; ?></span><br>
+
+            <input class=" inputField" type="password" id="password" name="password" placeholder="Password"><br>
+            <span class="errorMsg"><?php echo $passwordError; ?></span><br>
+
+            <!-- <input class="submitBtn" type="submit" name="submit" value="Submit"> -->
+            <button class="submitBtn" type="submit" name="submit" value="Submit">Submit</button><br>
+            <span class="successMsg"><?php echo $userAddedSuccess; ?></span><br>
+
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ readData.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>User Data</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">User Data</h1>
+    <?php
+    $dbServerName = "localhost";
+    $dbUserName = "root";
+    $dbPassword = "";
+    $dbName = "userdata";
+
+    $connection = new mysqli($dbServerName, $dbUserName, $dbPassword, $dbName);
+
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    $sql = "SELECT * FROM users";
+    $result = $connection->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<div class='readDataCard'>";
+        echo "<table><tr><td>ID</td><th>First Name</th><th>Last Name</th><th>Email Id</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] . "</td><td>" . $row["email"] . "<button class='deleteBtn' type='submit'>Delete</button><button class='editBtn' type='submit'>Edit</button></td></tr>";
+        }
+        echo "</table>";
+        echo "</div>";
+    } else {
+        echo "0 Result";
+    }
+
+    $connection->close();
+
+    ?>
+
+</body>
+
+</html>
