853f8061763c3f16d5e222deb03dff7145f2e425
Dharmendar1123 <dharmendar1123@gmail.com>
2022-03-23 14:03:51 +0530

    added encrypt decrypt logic

--- encrypt.php
+++ encrypt.php
@@ -1,17 +1,18 @@
 <?php
-function encrypt($sData)
+function encrypt($Data)
 {
-    $id = (float)$sData * 525325.24;
+    $id = (float)$Data * 525325.24;
     return base64_encode($id);
 }
 
-function decrypt($sData)
+function decrypt($Data)
 {
-    $url_id = base64_decode($sData);
+    $url_id = base64_decode($Data);
     $id = (float)$url_id / 525325.24;
     return $id;
 }
 
-function passwordEncode($sData) {
-    return password_hash($sData, PASSWORD_DEFAULT);
+function passwordEncode($Data)
+{
+    return password_hash($Data, PASSWORD_DEFAULT);
 }
